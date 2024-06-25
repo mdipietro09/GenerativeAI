a263db10546f1efe18ff7328c6c5ebf6114607d5
mauro <mauro@flieber.com>
2024-06-25 15:13:03 +0800

    .

---
+++ Chatbot/chatbot.py
@@ -0,0 +1,95 @@
+###############################################################################
+#                             CHATBOT                                         #
+###############################################################################
+# Setup
+## for speech-to-text
+import speech_recognition as sr
+
+## for text-to-speech
+from gtts import gTTS
+
+## for language model
+import ollama
+model = "maya"
+'''
+/set system Your name is Maya. You are a spoiled russian girl with a nasty attitude and strong accent. You also have dry humor and you always make fun of everyone when answering the questions. You like to keep your answers very short so you stop after the first sentence.
+'''
+
+## for data
+import os
+from datetime import datetime
+import numpy as np
+
+
+# Build the AI
+class ChatBot():
+    def __init__(self, name):
+        print("--- starting up", name, "---")
+        self.name = name.lower()
+
+    def speech_to_text(self):
+        recognizer = sr.Recognizer()
+        with sr.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=1)
+            print("listening...")
+            audio = recognizer.listen(mic)
+        try:
+            self.text = recognizer.recognize_google(audio)
+            print("me --> ", self.text)
+        except:
+            print("me -->  ERROR")
+
+    @staticmethod
+    def text_to_speech(text):
+        print("ai --> ", text)
+        speaker = gTTS(text=text, lang="en", slow=False)
+        speaker.save("res.mp3")
+        os.system("afplay res.mp3")  #mac->afplay | windows->start
+        os.remove("res.mp3")
+
+    ## predetermined commands
+    def wake_up(self, text):
+        lst = ["wake up "+self.name, self.name+"wake up ", "hey "+self.name]
+        return True if any(i in ai.text.lower() for i in lst) else False
+
+    def what(self, text):
+        lst = ["what are you", "who are you"]
+        return True if any(i in ai.text.lower() for i in lst) else False
+
+    @staticmethod
+    def action_time():
+        return datetime.now().time().strftime('%H:%M')
+
+
+# Run the AI
+if __name__ == "__main__":
+    
+    ai = ChatBot(name="Maya")
+    
+    while True:
+        ai.speech_to_text()
+
+        ## wake up
+        if ai.wake_up(ai.text) is True:
+            res = "Hello I am Maya the AI, what can I do for you?"
+
+        ## what
+        elif ai.what(ai.text) is True:
+            res = "I am an AI created by Mauro"
+
+        ## action time
+        elif "time" in ai.text:
+            res = ai.action_time()
+        
+        ## respond politely
+        elif any(i in ai.text for i in ["thank","thanks"]):
+            res = np.random.choice(["you're welcome!","anytime!","no problem!","cool!","I'm here if you need me!","peace out!"])
+        
+        ## conversation
+        else:   
+            res = ollama.generate(model=model, prompt=ai.text)["response"]
+            res = res.split("\n")[0]
+            #for trash in ["answer:", "support=", "**Note:", "(Note:", "- Reply:", "- reply:", "===", "(", "-output:", "Response:", "Maya: "]:
+            #    res = res[0:res.index(trash)].strip() if trash in res else res
+
+        ai.text_to_speech(res)
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Generative Artificial Intelligence with Python
 
+In this repo you can find useful examples of GenAI & LLM applications.\
+All the projects are documented on [*Medium*](https://maurodp.medium.com/).
+
+<br>
+
+- <ins>**[LLM Chatbot]()**</ins>
+<br>	
+	<p align="center"><img src="_docs/rag.gif" width="450" height="250"></p>
+<br>
+
+- <ins>**[RAG]()**</ins>
+<br>
+	<p align="center"><img src="_docs/rag.gif" width="550" height="250"></p>
+<br>
